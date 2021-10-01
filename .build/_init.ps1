29709506be94b568f7b219cdf5e3c4945252162b
Steve Brown <steve.brown+github@brownserve.co.uk>
2021-10-01 11:50:34 +0100

    Create a basic _init script

---
+++ .build/_init.ps1
@@ -0,0 +1,44 @@
+[CmdletBinding()]
+param ()
+$ErrorActionPreference = 'Stop'
+Write-Host "Initializing repository, please wait..."
+$Global:RepoBuildDirectory = Get-Item $PSScriptRoot -Force # Force is needed for dot-dirs on *nix
+
+# Ephemeral directories
+$EphemeralDirectories = @(
+    ($RepoBuildOutputDirectory = Join-Path $Global:RepoBuildDirectory 'output'),
+    ($RepoLogDirectory = Join-Path $Global:RepoBuildDirectory '.log')
+)
+
+try
+{
+    Write-Verbose "Recreating ephemeral directories"
+    $EphemeralDirectories | ForEach-Object {
+        if ((Test-Path $_))
+        {
+            Write-Verbose "Removing $_"
+            Remove-Item $_ -Force -Recurse -Confirm:$false | Out-Null
+        }
+        New-Item $_ -ItemType Directory -Force | Out-Null
+    }
+}
+catch
+{
+    Write-Error "Failed to set-up ephemeral directories.`n$($_.Exception.Message)"
+}
+
+# Now the paths should definitely exist and we can store them in global variables.
+# As we're storing them globally we convert their paths to make sure they are super compatible with things
+$Global:RepoLogDirectory = $RepoLogDirectory | Convert-Path
+$Global:RepoBuildOutputDirectory = $RepoBuildOutputDirectory | Convert-Path
+
+# Import the PowerShell module
+Write-Verbose "Importing repo module"
+try
+{
+    Import-Module (Join-Path $Global:RepoBuildDirectory 'module' 'os-provisioning.psm1') -Force
+}
+catch
+{
+    Write-Error "Failed to import the repo's PowerShell module.`n$($_.Exception.Message)"
+}
