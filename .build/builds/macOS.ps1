c0318ac23442ccce47e4fb4f6e7f7739a6031124
Steve Brown <steve.brown+github@brownserve.co.uk>
2021-10-06 00:44:16 +0100

    More work on macOS build

--- .build/builds/macOS.ps1
+++ .build/builds/macOS.ps1
@@ -3,7 +3,15 @@
     Builds our macOS images
 #>
 [CmdletBinding()]
-param ()
+param
+(
+    # The path to PyCreateUserPkgPath
+    [Parameter(
+        Mandatory = $false
+    )]
+    [string]
+    $PyCreateUserPkgPath
+)
 # Always stop on errors
 $ErrorActionPreference = 'Stop'
 
@@ -14,16 +22,14 @@ if (!$IsMacOS)
 
 Write-Host "Starting build $($MyInvocation.MyCommand)"
 
-# dot source the _init.ps1 script
-try
+# We don't dot source the _init script in this build as we run as root and it creates problems with
+# our ephemeral paths :(
+# We could potentially lock this build behind a CI/CD moniker if we need to, but for now let's just try running
+# _init before all our builds :)
+
+if (!$Global:RepoRootDirectory)
 {
-    Write-Verbose "Initialising repo"
-    $initScriptPath = Join-Path $PSScriptRoot '..' '_init.ps1' | Convert-Path
-    . $initScriptPath
-}
-catch
-{
-    Write-Error "Failed to init repo.`n$($_.Exception.Message)"
+    throw "Cannot find '`$global:RepoRootDirectory' have you run the _init.ps1 script?"
 }
 
 # First we need to get the list of macOS versions we currently build for
@@ -55,6 +61,39 @@ $macOSVersions | ForEach-Object {
         Build-MacOSPackage -PackageProjectPath $PackagesToBuild -OutputDirectory $script:PackerFilesDirectory -Verbose:($PSBoundParameters['Verbose'] -eq $true) | Out-Null
     }
 
+    # Create the "packer" user by running pycreateuserpkg, we only do this if we've been given a path to the python script
+    if ($PyCreateUserPkgPath)
+    {
+        Write-Verbose "Updating packer_user.pkg"
+        if (!(Test-Path $PyCreateUserPkgPath))
+        {
+            throw "Cannot find pycreateuserpkg at $PyCreateUserPkgPath"
+        }
+        try
+        {
+            Write-Verbose "Building packer_user.pkg"
+            $PackerPackagePath = Join-Path $Global:RepoBuildOutputDirectory 'packer_user.pkg'
+            Start-SilentProcess `
+                -FilePath $PyCreateUserPkgPath `
+                -ArgumentList "-n packer -f packer -p packer -u 525 -V 1 -i com.brownserveuk.packer -a -A -d $PackerPackagePath"
+        }
+        catch
+        {
+            Write-Error "Failed to update packer_user.pkg.`n$($_.Exception.Message)"
+        }
+
+        # Now copy it over to the relevant place
+        try
+        {
+            Write-Verbose "Copying packer_user.pkg to packer directory"
+            Copy-Item $PackerPackagePath -Destination $script:PackerFilesDirectory -Force
+        }
+        catch
+        {
+            Write-Error "Failed to copy packer_user.pkg.`n$($_.Exception.Message)"
+        }
+    }
+
     # If we've got custom scripts we'll need to copy those over too
     $ScriptsDir = $VersionChildItems | Where-Object { $_.Name -eq 'scripts' } | Select-Object -ExpandProperty PSPath | Convert-Path
     if ($ScriptsDir)
@@ -64,9 +103,9 @@ $macOSVersions | ForEach-Object {
     }
 
     # Now we can run the packer build(s)
-    $PackerTemplates = Get-ChildItem $VersionChildItems | Where-Object {$_.Name -match ".hcl|.json"} | Select-Object -ExpandProperty PSPath | ForEach-Object {
-        Invoke-PackerValidate (Convert-Path $_)
-        Invoke-PackerBuild (Convert-Path $_)
+    Get-ChildItem $VersionChildItems | Where-Object { $_.Name -match ".hcl|.json" } | Select-Object -ExpandProperty PSPath | ForEach-Object {
+        Invoke-PackerValidate (Convert-Path $_) -WorkingDirectory $BuildOutputDirectory
+        Invoke-PackerBuild (Convert-Path $_) -WorkingDirectory $BuildOutputDirectory
     }
 }
 
