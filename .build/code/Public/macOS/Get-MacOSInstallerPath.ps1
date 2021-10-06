2ecc85c28ad4a0796e5f1fe659af5c69e1441e40
Steve Brown <steve.brown+github@brownserve.co.uk>
2021-10-06 15:13:36 +0100

    Add a new cmdlet

---
+++ .build/code/Public/macOS/Get-MacOSInstallerPath.ps1
@@ -0,0 +1,44 @@
+<#
+.SYNOPSIS
+    Simple cmdlet for checking and returning the path to macOS installers
+.DESCRIPTION
+    Simple cmdlet for checking and returning the path to macOS installers
+    This ensures we can easily get the path to the installer across versions
+.EXAMPLE
+    Get-MacOSInstallerPath -macOSVersion '11'
+    
+    This would get the macOS installer path for macOS 11
+#>
+function Get-MacOSInstallerPath
+{
+    [CmdletBinding()]
+    param
+    (
+        # macOS
+        [Parameter(Mandatory = $true, Position = 0)]
+        [string]
+        $macOSVersion
+    )
+    
+    switch ($macOSVersion)
+    {
+        '11'
+        {
+            $InstallerPath = '/Applications/Install macOS Big Sur.app'
+        }
+        Default
+        {
+            Write-Error "Unsupported macOS version '$macOSVersion'"
+            Return $null
+        }
+    }
+    if (!(Test-Path $InstallerPath))
+    {
+        Write-Error "Cannot find installer at '$InstallerPath' for macOS '$macOSVersion'"
+        Return $null
+    }
+    else
+    {
+        Return $InstallerPath
+    }
+}
