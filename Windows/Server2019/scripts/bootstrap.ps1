d0e291f2cbc0221e7fcc5ae003ccae278f9b77ea
Steve Brown <steve.brown+github@brownserve.co.uk>
2021-10-07 17:59:23 +0100

    I wonder if chocolatey will be available at this point?

--- Windows/Server2019/scripts/bootstrap.ps1
+++ Windows/Server2019/scripts/bootstrap.ps1
@@ -14,6 +14,10 @@ $ErrorActionPreference = 'Stop'
 # We need chocolatey so we can grab PSExec
 Install-Chocolatey
 
+# So doing a sysprep kills the network driver which obviously kills WinRM...
+# So we'll run it as the shutdown command in Packer, we'll need to do this as SYSTEM though which requires PSEXEC.
+choco install psexec --confirm --ignore-checksums
+
 ### THIS SHOULD ALWAYS BE THE LAST STEP ###
 Enable-WinRM
 ### This is because once WinRM is enabled it signals packer to continue on with provisioning. ###
