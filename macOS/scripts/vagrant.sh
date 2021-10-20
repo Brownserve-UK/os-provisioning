0a13822bd1a00844b2e44fac72fb6f0f38750594
Steve Brown <steve.brown+github@brownserve.co.uk>
2021-10-20 18:02:57 +0100

    forgot this!

--- macOS/scripts/vagrant.sh
+++ macOS/scripts/vagrant.sh
@@ -8,3 +8,6 @@ sudo dscl . -create /Users/vagrant PrimaryGroupID 1000
 sudo dscl . -create /Users/vagrant NFSHomeDirectory /Local/Users/vagrant
 sudo dscl . -passwd /Users/vagrant vagrant
 sudo dscl . -append /Groups/admin GroupMembership vagrant
+
+# Add the 'vagrant' user we created earlier to the sudoers list and make it so they don't need a password to sudo.
+echo 'vagrant     ALL=(ALL)       NOPASSWD: ALL' >/private/etc/sudoers.d/vagrant
