ce14662ef47ba759d961a4c4943fb5d63b1665fd
Steve Brown <steve.brown+github@brownserve.co.uk>
2021-10-22 23:18:55 +0100

    Forgot to commit these

--- Linux/Ubuntu1804/scripts/clear-machineid.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-set -e
-set -x
-
-# Clear /etc/machine-id so that it is recreated once the VM next boots.
-# Do not delete the file as it is required for systemd.
-echo "" > /etc/machine-id
